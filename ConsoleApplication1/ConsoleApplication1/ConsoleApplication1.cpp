f90719a126655d42055cd13ea8149173c839418c
Sena Sümer <ssena6050@gmail.com>
2022-03-21 00:29:32 +0300

    sdsf

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using  namespace std;
+int array[9];
+
+
+void rotation(int array[], int arraysize)
+{
+	int i;
+	
+
+
+int main() {
+
+
+
+	string N;
+	string direction;
+	int array[] = { 1,2,3,4,5,6,7,8,9 };
+	for (int i = 0; i < 9; i++)
+	{
+		cout << array[i] << " ";
+	}
+
+	
+	system("pause");
+}
