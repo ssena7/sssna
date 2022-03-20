88970725d5a9e2c34e35b82c1aaaffa80cabc3e4
Sena Sümer <ssena6050@gmail.com>
2022-03-21 00:35:20 +0300

    write a loop

--- homeworkdeneme1/homeworkdeneme1/homeworkdeneme1.cpp
+++ homeworkdeneme1/homeworkdeneme1/homeworkdeneme1.cpp
@@ -5,6 +5,17 @@ int array[9];
 
 void rotation(int array[], int arraysize)
 {
+	string N;
+	string direction;
+	int array[] = { 1,2,3,4,5,6,7,8,9 };
+	for (int i = 0; i < 9; i++)
+	{
+		cout << array[i] << " ";
+	}
+
+	cout << endl;
+	cout << "Enter N for shift an array:" << endl;
+	cin >> N;
 }
 
 
