e326eed67164782e1d5fe11718647bae0675c77a
ssena7 <101149488+ssena7@users.noreply.github.com>
2022-03-20 23:54:36 +0300

    Add files via upload

---
+++ 01.cpp
@@ -0,0 +1,13 @@
+﻿'Project5.exe' (Win32): Loaded 'C:\Users\Selçuk\source\repos\Project5\Debug\Project5.exe'. Symbols loaded.
+'Project5.exe' (Win32): Loaded 'C:\Windows\SysWOW64\ntdll.dll'. 
+'Project5.exe' (Win32): Loaded 'C:\Windows\SysWOW64\kernel32.dll'. 
+'Project5.exe' (Win32): Loaded 'C:\Windows\SysWOW64\KernelBase.dll'. 
+'Project5.exe' (Win32): Loaded 'C:\Windows\SysWOW64\msvcp140d.dll'. 
+'Project5.exe' (Win32): Loaded 'C:\Windows\SysWOW64\vcruntime140d.dll'. 
+'Project5.exe' (Win32): Loaded 'C:\Windows\SysWOW64\ucrtbased.dll'. 
+The thread 0x7ec has exited with code 0 (0x0).
+The thread 0x3820 has exited with code -1073741510 (0xc000013a).
+The thread 0x345c has exited with code -1073741510 (0xc000013a).
+The thread 0x35c8 has exited with code -1073741510 (0xc000013a).
+The thread 0x1f8c has exited with code -1073741749 (0xc000004b).
+The program '[14472] Project5.exe' has exited with code -1073741510 (0xc000013a).
