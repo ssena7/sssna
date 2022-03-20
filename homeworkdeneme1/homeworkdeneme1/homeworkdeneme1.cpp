024e5ef507f74f3f523c1e5ffe2c6e341f237dab
Sena Sümer <ssena6050@gmail.com>
2022-03-21 00:34:57 +0300

    write a function.

---
+++ homeworkdeneme1/homeworkdeneme1/homeworkdeneme1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using  namespace std;
+int array[9];
+
+
+void rotation(int array[], int arraysize)
+{
+}
+
+
+int main() {
+
+
+
+	cout << "Enter direction for shift an array:" << endl;
+	cin >> direction;
+
+
+
+	system("pause");
+}
